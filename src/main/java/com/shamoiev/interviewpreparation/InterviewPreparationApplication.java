cd4a7ed0c2f45a37ff93b3684e4a90cc48631638
Oleksandr Shamoiev <wamoev@gmail.com>
2023-02-18 12:40:33 +0200

    Initial commit

---
+++ src/main/java/com/shamoiev/interviewpreparation/InterviewPreparationApplication.java
@@ -0,0 +1,13 @@
+package com.shamoiev.interviewpreparation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPreparationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewPreparationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shamoiev/interviewpreparation/InterviewPreparationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shamoiev.interviewpreparation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPreparationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
