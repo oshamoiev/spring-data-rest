0badd50db82dda0749c7278f026d766e8538a6b5
Oleksandr Shamoiev <wamoev@gmail.com>
2023-02-18 20:47:49 +0200

    Make BoardRepository JpaRepository

--- src/main/java/com/shamoiev/interviewpreparation/repository/BoardRepository.java
+++ src/main/java/com/shamoiev/interviewpreparation/repository/BoardRepository.java
@@ -1,10 +1,10 @@
 package com.shamoiev.interviewpreparation.repository;
 
 import com.shamoiev.interviewpreparation.entity.Board;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface BoardRepository extends CrudRepository<Board, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long> {
     List<Board> findAllBySnowboarderId(Long id);
 }
