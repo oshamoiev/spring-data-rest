4a78e5aabef423dff72ca69f5227a669eceaae53
Oleksandr Shamoiev <wamoev@gmail.com>
2023-02-18 17:58:20 +0200

    Add resource controllers

---
+++ src/main/java/com/shamoiev/interviewpreparation/controller/BoardController.java
@@ -0,0 +1,29 @@
+package com.shamoiev.interviewpreparation.controller;
+
+import com.shamoiev.interviewpreparation.entity.Board;
+import com.shamoiev.interviewpreparation.service.BoardService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/snowboarders/{phoneNumber}/boards")
+public class BoardController {
+    private final BoardService boardService;
+
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @GetMapping
+    public List<Board> getAllBoardsBySnowboarder(@PathVariable String phoneNumber) {
+        return boardService.getAllBoardsBySnowboarder(phoneNumber);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Board createBoard(@PathVariable String phoneNumber, @RequestBody Board board) {
+        return boardService.createBoard(phoneNumber, board);
+    }
+}
---
+++ src/main/java/com/shamoiev/interviewpreparation/controller/SnowboarderController.java
@@ -0,0 +1,33 @@
+package com.shamoiev.interviewpreparation.controller;
+
+import com.shamoiev.interviewpreparation.entity.Snowboarder;
+import com.shamoiev.interviewpreparation.service.SnowboarderService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/snowboarders")
+public class SnowboarderController {
+
+    private final SnowboarderService snowBoarderService;
+
+    public SnowboarderController(SnowboarderService snowBoarderService) {
+        this.snowBoarderService = snowBoarderService;
+    }
+
+    @GetMapping
+    public List<Snowboarder> getAllSnowboarders() {
+        return snowBoarderService.getAll();
+    }
+
+    @GetMapping("{phoneNumber}")
+    public Snowboarder getSnowboarder(@PathVariable String phoneNumber) {
+        return snowBoarderService.getSnowBoarder(phoneNumber);
+    }
+
+    @PostMapping
+    public Snowboarder createSnowboarder(@RequestBody Snowboarder snowboarder) {
+        return snowBoarderService.createSnowBoarder(snowboarder);
+    }
+}
