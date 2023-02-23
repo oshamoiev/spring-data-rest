a869466c830ce23e2ec90acf7691068ff2d6f897
Oleksandr Shamoiev <wamoev@gmail.com>
2023-02-23 16:31:48 +0200

    Move TestController

--- src/main/java/com/shamoiev/interviewpreparation/controller/TestThymeleafController.java
+++ src/main/java/com/shamoiev/interviewpreparation/controller/TestThymeleafController.java
@@ -1,4 +1,4 @@
-package com.shamoiev.interviewpreparation.controller.api;
+package com.shamoiev.interviewpreparation.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
